f4d86270d9a55be8791c2e1e3d8d46e1573b0eb9
Jonah George <jonah.george@me.com>
2015-12-06 12:26:02 -0800

    Scaffold ActiveJobs for CourseCatalog syncing
    rails g job departments_sync
    rails g job courses_sync
    rails g job sections_sync

---
+++ app/jobs/courses_sync_job.rb
@@ -0,0 +1,7 @@
+class CourseSyncJob < ActiveJob::Base
+  queue_as :default
+
+  def perform(*args)
+    # Do something later
+  end
+end
---
+++ spec/jobs/courses_sync_job_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe CourseSyncJob, type: :job do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
