227fceb854ae84fe3d4328804c35bf0b381a3cc7
Jonah George <jonah.george@me.com>
2016-03-02 08:38:33 -0800

    Administrators can edit user's FTE

--- app/controllers/administrator/users_controller.rb
+++ app/controllers/administrator/users_controller.rb
@@ -25,7 +25,7 @@ module Administrator
       @user = User.new(user_params)
 
       if @user.save
-        redirect_to administrators_user_path(@user), notice: 'User was successfully created.'
+        redirect_to administrator_user_path(@user), notice: 'User was successfully created.'
       else
         render :new
       end
@@ -34,7 +34,7 @@ module Administrator
     # PATCH/PUT /admin/users/1
     def update
       if @user.update(user_params)
-        redirect_to administrators_user_path(@user), notice: 'User was successfully updated.'
+        redirect_to administrator_user_path(@user), notice: 'User was successfully updated.'
       else
         render :edit
       end
@@ -43,7 +43,7 @@ module Administrator
     # DELETE /admin/users/1
     def destroy
       @user.destroy
-      redirect_to administrators_users_url, notice: 'User was successfully destroyed.'
+      redirect_to administrator_users_url, notice: 'User was successfully destroyed.'
     end
 
     private
@@ -54,7 +54,7 @@ module Administrator
 
       # Only allow a trusted parameter "white list" through.
       def user_params
-        params[:user].permit(:is_administrator)
+        params[:user].permit(:is_administrator, :fte)
       end
   end
 end
