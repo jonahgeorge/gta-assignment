892fc8889eb2f7b6db4366ebe15174df9d29d36f
Jonah George <jonah.george@me.com>
2016-03-04 11:38:41 -0800

    Clean up UI

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
 
   root 'pages#index'
-  get 'about' => 'pages#about'
 
   devise_for :users, :controllers => {
     :omniauth_callbacks => "users/omniauth_callbacks"
