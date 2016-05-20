58843bbd2e85f5f3471e85bdd6c685b0136d964f
Jonah George <jonah.george@me.com>
2016-05-19 23:19:41 -0700

    Save assignments

--- app/controllers/administrator/assignments_controller.rb
+++ app/controllers/administrator/assignments_controller.rb
@@ -2,35 +2,71 @@ require 'integer_linear_program'
 
 module Administrator
   class AssignmentsController < BaseController
+    before_action :set_students, only: [:new, :create, :edit, :update, :show]
+    before_action :set_sections, only: [:new, :create, :edit, :update, :show]
 
     def index
-      @assignments = Assignment.all
+      @assignments = Assignment.all.order(created_at: :desc)
+    end
+
+    def new
+      @assignment = Assignment.new
+      @pairs = {}
+    end
+
+    def edit
+      @assignment = Assignment.find(params[:id])
+      @pairs = @assignment.assignments
+    end
+
+    def create
+      if params[:commit] == "Save"
+        @pairs = params[:assignments] || {}
+      elsif params[:commit] == "Run solver"
+        problem = ::IntegerLinearProgram.new(@students, @sections, params[:assignments])
+        problem.solve
+        @pairs = problem.results
+      end
+
+      @assignment = Assignment.create(assignments: @pairs)
+      redirect_to administrator_assignment_path(@assignment)
+    end
+
+    def update
+      if params[:commit] == "Save"
+        @pairs = params[:assignments] || {}
+      elsif params[:commit] == "Run solver"
+        problem = ::IntegerLinearProgram.new(@students, @sections, params[:assignments])
+        problem.solve
+        @pairs = problem.results
+      end
+
+      @assignment = Assignment.create(assignments: @pairs)
+      redirect_to administrator_assignment_path(@assignment)
     end
 
     def show
       @assignment = Assignment.find(params[:id])
       @pairs = @assignment.assignments
 
-      @students = User.includes([{section_preferences: :section}, :experiences]).students
-      @sections = Section.includes([{course: [:requirements, :department]}, {instructor: :student_preferences}, :section_preferences]).with_current_term
+      respond_to do |format|
+        format.html
+        format.txt  { render 'show.txt.erb' }
+        format.xlsx { render xlsx: 'show.xlsx.axlsx' }
+      end
     end
 
-    def create
-      @students = User.includes([{section_preferences: :section}, :experiences]).students
-      @sections = Section.includes([{course: [:requirements, :department]}, {instructor: :student_preferences}, :section_preferences]).with_current_term
-      @pairs = params[:assignments]
+    private
 
-      if params[:commit] == "Export to text"
-        render xlsx: 'index.xlsx.axlsx'
-      elsif params[:commit] == "Export to pdf"
-        render xlsx: 'index.xlsx.axlsx'
-      else
-        problem = ::IntegerLinearProgram.new(@students, @sections, params[:assignments])
-        problem.solve
-        @pairs = problem.results
-        @assignment = Assignment.create! assignments: @pairs
-        redirect_to administrator_assignments_path(@assignment)
-      end
+    def set_students
+      @students = User.includes([{section_preferences: :section}, :experiences]).gtas
+    end
+
+    def set_sections
+      @sections = Section.includes(
+        {course: [:requirements, :department]},
+        {instructor: :student_preferences}, :section_preferences)
+        .with_current_term
     end
   end
 end
